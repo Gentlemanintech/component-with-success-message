f73cab381c65789af4007d2d5f06708dc7bc6d96
Gentlemanintech <108980260+Gentlemanintech@users.noreply.github.com>
2023-08-03 09:55:08 -0700

    Update on script and index file
    I added some class to the html element and started working on the script file

---
+++ script.js
@@ -0,0 +1,8 @@
+let submitButton = document.getElementById('submit-btn');
+let container = document.getElementById('container');
+
+
+submitButton.addEventListener('click', e => {
+    e.preventDefault();
+    
+})
